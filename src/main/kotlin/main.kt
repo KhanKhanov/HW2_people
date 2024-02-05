546a8bece3c2d41efee25817033654127f260268
Neto test <buter_98@mail.ru>
2024-02-05 07:30:57 +0300

    Изменение условий проверки в файле main

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,9 @@
 fun main() {
-    val likes = 101
+    val likes = 111
     val lastNumber = likes % 10
+    val secondNumber = likes % 100
 
-    val result = if (lastNumber == 1) "человеку" else "людям"
+    val result = if (lastNumber == 1 && secondNumber != 11)  "человеку"  else "людям"
     println("Понравилось $likes $result")
 
 }
